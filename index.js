7beb66ed756cb5274f4183a022d3a7f0b798e8de
Muhanned Al Risi <muhanned@Muhanneds-MacBook-Pro.local>
2020-12-03 13:30:57 +0400

    fix and added unit test

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const ThawaniClient = require('./dist/app.js')
-require('dotenv').config()
+const ThawaniClient = require("./dist/app.js");
 
-class a {} 
-console.log(a)
-// console.log(app)
-let x  = new ThawaniClient(process.env.SECRET , process.env.PUBLISH, process.env.ENV)
-console.log(x.get_endpoint())
-// console.log(x.getCustomers());
+require("dotenv").config();
+
+const api = new ThawaniClient(
+  process.env.SECRET,
+  process.env.PUBLISH,
+  process.env.ENV
+);
+
+// api
+//   .find_session("checkout_940RmwSubopVnPj5HadK6NZ5blpUdrx0mCSOGPqDd5YVei8eTS")
+//   .then((data) => {
+//     console.log(data.data);
+//   })
+//   .catch((err) => console.error(err));
+module.exports = ThawaniClient;
