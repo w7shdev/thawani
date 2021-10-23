9eedfaf81c7ad4eb0835e1636189c306d32ed2ec
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2021-10-23 23:32:59 +0400

    implement find method to get the plan information

--- src/endpoints/plans.ts
+++ src/endpoints/plans.ts
@@ -13,8 +13,18 @@ export default class Plans {
     }
 
     public async create(){}
-
-    public async find(){}
+    /**
+     * This endpoint is used to get the information about a single plan
+     * that has been previously created.
+     * @Endpoint api/v1/plans/
+     * @http_method GET
+     * @param {string} plan_id  plans ID
+     * @return {Promise} response 
+     */
+    public async find(plan_id : string) : Promise<any>{
+        const {data} = await this.axios.get('api/v1/plans/' + plan_id )
+        return data
+    }
 
     public async findAll(){}
 
